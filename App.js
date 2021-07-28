b23ee3441f595c5101201809af00024aee3fad56
jean-dedieu <jeandedieu.twagirumuhoza@gmail.com>
2021-07-28 11:58:04 +0200

    styling the alert form

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding:50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Décrivez votre signalement"
+                   style={{ width:'80%', borderColor: '#53c7b8', borderWidth: 1, padding:10}}
+        />
+        <Button title=">" color="#53c7b8" width="20%"/>
+      </View>
+      
+      <View>
+    
+      </View>
     </View>
   );
 }
