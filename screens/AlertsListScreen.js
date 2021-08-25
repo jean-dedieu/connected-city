3a4cc8faea214955d3e95df38e6506ab0e9a5831
jean-dedieu <jeandedieu.twagirumuhoza@gmail.com>
2021-08-25 11:44:39 +0200

    implemented alerts list screen to display all alerts

--- screens/AlertsListScreen.js
+++ screens/AlertsListScreen.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, Platform, FlatList } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import { render } from 'react-dom';
 import AlertItem from '../components/AlertItem';
+import * as alertsActions from '../store/alerts-actions';
 
 const AlertsListScreen = props => {
-        const alertPlaces = useSelector(state => state.alertPlaces.alertPlaces);
+        const alerts = useSelector(state => state.alerts.alerts);
+        /**
+         * Dispatching our alerts
+         * This is the only way to trigger our application state change.
+         *  With React Redux, our components will never access the store directly
+         * Connect does it for us.
+         * When the application starts up
+         */
+        const dispatch = useDispatch();
+
+        useEffect (() => {
+                dispatch(alertsActions.loadAlerts());
+
+        }, [dispatch]);
+      
         return (<FlatList
-                 data={alertPlaces}
-                 keyExtractor={ item => item.id}
-                 renderItem={itemData => <AlertItem image={null} title={itemData.item.title} address={null} onSelect={() => {
+                 data={alerts}
+                 keyExtractor={ item => item.id }
+                 renderItem={itemData => (
+                 <AlertItem image={null} title={itemData.item.title} description={itemData.item.description} address={null} onSelect={() => {
                          props.navigation.navigate('AlertDetail', {alertTitle: itemData.item.title, alertId: itemData.item.id});
                  }}/>
-                }
-                />
+                 )} />
         );
-};
+     };
 
 AlertsListScreen.navigationOptions = navData => {
        return { 
